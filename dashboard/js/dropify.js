46e32152959d0fe408685a9b7ec51e82f1661f63
sajalroy2020 <sajal2020roy@gmail.com>
2023-02-28 09:33:31 +0600

    clz

---
+++ dashboard/js/dropify.js
@@ -0,0 +1,50 @@
+$(function() {
+  'use strict';
+
+  $('#myDropify').dropify();
+});
+
+
+
+$(function() {
+  'use strict';
+
+  $('#myDropify2').dropify();
+});
+
+$(function() {
+  'use strict';
+
+  $('#myDropify3').dropify();
+});
+
+
+$(function() {
+  'use strict';
+
+  $('#myDropify4').dropify();
+});
+
+
+$(function() {
+  'use strict';
+
+  $('#myDropify5').dropify();
+});
+
+
+$(function() {
+  'use strict';
+
+  $('#myDropify6').dropify();
+});
+
+
+$(function() {
+  'use strict';
+
+  $('#myDropify7').dropify();
+});
+
+
+
